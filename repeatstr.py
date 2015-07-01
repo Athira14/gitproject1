25bf8f23d557e4e9bf55a9cb065377df0e69bee7
Athira14 <athirabalachandran333@gmail.com>
2015-07-01 16:11:01 +0530

    initial commit

---
+++ repeatstr.py
@@ -0,0 +1,11 @@
+﻿#printing the repeated characters in a string
+string=raw_input("enter the string")
+count={}
+for char in string:
+ if char in count.keys():
+    count[char] +=1
+ else:
+    count[char] =1
+      
+print (count)
+      
